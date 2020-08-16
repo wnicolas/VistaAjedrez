e2d61fbb7f88fbeb0089e8d0762a5f1972370819
wnbuitragoc <wnbuitragoc@correo.udistrital.edu.co>
2020-08-15 20:59:55 -0500

    Registro y generación de tablero completada

--- src/app/tablero/tablero.component.ts
+++ src/app/tablero/tablero.component.ts
@@ -10,9 +10,35 @@ export class TableroComponent implements OnInit {
   player1:String;
   player2:String;
 
+  fila0=[];
+  fila1=[];
+  fila2=[];
+  fila3=[];
+  fila4=[];
+  fila5=[];
+  fila6=[];
+  fila7=[];
+
+  llenarTablero(){
+    for(var i=0;i<8;i++){
+      this.fila0[i]=new Casilla(0,i);
+      this.fila1[i]=new Casilla(1,i);
+      this.fila2[i]=new Casilla(2,i);
+      this.fila3[i]=new Casilla(3,i);
+      this.fila4[i]=new Casilla(4,i);
+      this.fila5[i]=new Casilla(5,i);
+      this.fila6[i]=new Casilla(6,i);
+      this.fila7[i]=new Casilla(7,i);
+    }
+  }
+  columnas=[this.fila0,this.fila1,this.fila2,this.fila3,this.fila4,this.fila5,this.fila6,this.fila7];
+  
+  
+
   constructor() { }
 
   ngOnInit(): void {
+    this.llenarTablero();
   }
 
   salvarJugadores(j1: String,j2: String){
@@ -21,4 +47,19 @@ export class TableroComponent implements OnInit {
     alert('Bienvenidos: ' + this.player1+" y "+this.player2);
   }
 
+}
+class Casilla{
+  posi:number;
+  posj: number;
+  rojas:boolean;
+  
+  constructor(posi:number,posj:number) { 
+    this.posi=posi;
+    this.posj=posj;
+    if((posi+posj)%2==0){
+      this.rojas=true;
+    }else{
+      this.rojas=false;
+    }
+  }
 }
